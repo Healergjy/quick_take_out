a1a6694b69f44952a1d6625d1afad3da71ae0f7d
甘嘉奕 <122898981@qq.com>
2023-03-23 22:52:31 +0800

    second commit

--- src/main/java/com/gjy/quick/controller/OrderController.java
+++ src/main/java/com/gjy/quick/controller/OrderController.java
@@ -9,9 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * 订单
- */
+
 @Slf4j
 @RestController
 @RequestMapping("/order")
